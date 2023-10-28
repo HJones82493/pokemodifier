d6e48c7cdfdf18227af4bcbb574f3990efcd6b97
hjones82493 <hjones@HuntersLaptop.attlocal.net>
2023-10-27 23:44:26 -0500

    Add item shop infor

--- gen_1/rom_map.py
+++ gen_1/rom_map.py
@@ -1,6 +1,10 @@
 class RomMap:
+	SHOP_START_VALUE = 0xFE
+	SHOP_END_VALUE = 0xFF
+	self._addresses = dict()
 	def __init__(self):
 		self._setup_rom_map()
 
 	def _setup_rom_map(self):
-		pass
+		self._addresses["shops"] = {"start": 0x2442, "end": 0x24D5, "size": 0}
+		self._addresses["item_prices"] = {"start": 0x4608, "end": 0x472A, "size": 2}
