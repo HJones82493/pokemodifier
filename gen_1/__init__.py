6595036e6d13aa9dca0151f5c4b0a64fb001ba31
hjones82493 <hjones@HuntersLaptop.attlocal.net>
2023-10-27 23:44:12 -0500

    Add basic items functionality

--- gen_1/__init__.py
+++ gen_1/__init__.py
@@ -2,3 +2,4 @@ from .character_encoder import CharacterEncoder
 from .character_decoder import CharacterDecoder
 from .rom_map import RomMap
 from .pokemon_info import PokemonInfo
+from .items import ItemInfo
